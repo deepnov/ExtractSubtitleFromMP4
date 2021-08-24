ab8a2e6d43037510319da46b82bc6ee8c4d2fa46
Deepak G <deepnov@users.noreply.github.com>
2021-08-24 19:53:08 +0530

    Update and rename ListDirFileNamesV2.py to extract_subtitle.py

--- extract_subtitle.py
+++ extract_subtitle.py
@@ -2,8 +2,8 @@ import os
 import sys
 import io
 
-def search(scriptparam,depth):
-    dirList = os.listdir("./")
+def listfiles(scriptparam,depth):
+    dirList = os.listdir()
     for filename in dirList:
         x=0
         holder=""
@@ -14,21 +14,24 @@ def search(scriptparam,depth):
         else:
             holder=holder+filename
         #print(depth) #was used for troubleshooting depth of subfiles/folders
+        if filename.endswith(".mp4"):
+            print(filename+"\n")
+            os.system('ffmpeg -hide_banner -i "'+filename+'" -map 0:s:0 "'+filename+'".srt')#ffmpeg tool should be installed in the machine before running this tool
+
+
         if (filename != scriptparam):
-            #print(holder)
-            FILEOUT.write(holder + "\n")
+            
             if os.path.isdir(filename):
                 #print(filename + " is a folder") #was used from troubleshooting if file was a folder or file
                 os.chdir(filename)
                 depth=depth+1
-                search(scriptparam, depth)
+                listfiles(scriptparam, depth)
                 depth=depth-1
                 os.chdir("..")
 
-FILEOUT=io.open("output.txt", "w", encoding='utf-8')
-location=input("Enter in the location of the directory you wish to search. \n")
-os.chdir(location)
 
-#copen output.txt file to write the list, excluding the python script name
-search(location,0)
-FILEOUT.close()
+location=os.getcwd()
+location=location+'\\'
+os.chdir(location)
+print(location)
+listfiles(location,0)
